28d3e5a2e0315700df69003b7785eee0edee325f
mahadihasan1555 <mahadihasan1555@gmail.com>
2020-07-23 22:11:21 +0600

    loops, for loop, while loop

---
+++ loop.js
@@ -0,0 +1,35 @@
+//while loop
+ var num = 0;
+ while (num < 25){
+    console.log(num);
+   // num = num + 1;
+   num++;
+ }
+
+var num = 10;
+while (num < 20 ){
+    console.log(num);
+    num++;
+}
+
+var num = 30;
+while (num <= 40 ){
+    console.log(num);
+    num++;
+}
+
+// for loop
+for ( var i = 0; i < 10; i++){
+    console.log(i);
+}
+
+for ( var i = 0; i <= 10; i++){
+    console.log(i);
+}
+
+var nums = [12, 22, 11, 42, 24,41];
+for (var i = 0; i < nums.length; i++){
+    var element = nums [i];
+    console.log(element);
+}
+
