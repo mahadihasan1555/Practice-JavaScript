f76ace04bcfe7e55413fadce558a8e34cf470493
mahadihasan1555 <mahadihasan1555@gmail.com>
2020-07-24 21:08:33 +0600

    calculate foctorial of a number

---
+++ factorial.js
@@ -0,0 +1,22 @@
+// 3! = 1*2*3
+// 4! = 1*2*3*4
+// 5! = 1*2*3*4*5
+
+var factorial = 1;
+for (var i = 1; i <= 5; i++){
+    factorial = factorial* i
+    console.log(i, factorial);
+}
+
+var factorial = 1;
+for (var i = 1; i <= 10; i++){
+    factorial = factorial* i
+    console.log(i, factorial);
+}
+
+var factorial = 1;
+for (var i = 1; i <= 15; i++){
+    factorial = factorial* i
+    console.log(i, factorial);
+}
+
