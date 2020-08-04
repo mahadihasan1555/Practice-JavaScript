99cd232169b25bb8af9f499f7e8f1dabe8fc8aa5
mahadihasan1555 <mahadihasan1555@gmail.com>
2020-08-04 21:04:56 +0600

    revise and practicing  inch to feet

--- factorialRecursive.js
+++ factorialRecursive.js
@@ -1,2 +1,38 @@
 // i can not understand at this moment
 // see you later
+
+// 1! = 1
+// 2! = 2 * 1 = 2
+// 3! = 3 * 2 * 1 = 6
+// 4! = 4 * 3 * 2 * 1 = 24
+// 5! = 5 * 4 * 3 * 2 * 1 = 120
+
+// n! = n * (n - 1) * (n - 2) * ...*1
+
+
+
+function factorial(x) 
+{ 
+
+  if (x === 0)
+ {
+    return 1;
+ }
+  return x * factorial(x-1);
+         
+}
+console.log(factorial(5));
+
+
+function factorial(n){
+    //base case
+    if(n == 0 || n == 1){
+        return 1;
+    //recursive case
+    }else{
+        return n * factorial(n-1);
+    }
+}
+let n = 4;
+answer = factorial(n)
+console.log("The factorial of " + n + " is " + answer);
--- inchToFeet.js
+++ inchToFeet.js
@@ -18,4 +18,47 @@ console.log(brotherFeet);
 var motherFeet = inchToFeet(450);
 console.log(motherFeet);
 
+//inch to feet
 
+// variable
+
+var inch = 100;
+var feet = inch / 12;
+console.log(feet);
+console.log(feet);
+console.log(feet);
+
+
+var inch = 23;
+var feet = inch / 12;
+console.log(feet);
+
+var inch = 12432;
+var feet = inch / 12;
+console.log(feet);
+
+
+//function
+
+function inchToFeet(inch){
+    var feet = inch / 12;
+    return feet;
+}
+var myFeet = inchToFeet(199);
+console.log(myFeet);
+
+var mahadiFeet = inchToFeet(222);
+console.log(mahadiFeet);
+
+//loop
+var junior = [ 121, 422, 523, 231 ];
+var juniorFeet = inchToFeet(junior[1]);
+console.log(juniorFeet);
+
+
+function inchToFeet(inch){
+    var feet = inch / 12;
+    return feet;
+}
+var nameFeet = inchToFeet(23233);
+console.log(nameFeet);
