dbc8bfb1c4d9f4b42d3c463f9061a7e94e5d087d
mahadihasan1555 <mahadihasan1555@gmail.com>
2020-08-06 22:33:58 +0600

    revise and practicing factorial number

--- factorial.js
+++ factorial.js
@@ -20,3 +20,26 @@ for (var i = 1; i <= 15; i++){
     console.log(i, factorial);
 }
 
+//for loop
+for ( let i = 0; i<= 10; i++){
+    console.log(i);
+}
+
+var factorial = 1;
+for (var i = 1; i<=5; i++){
+    factorial = factorial * i;
+    console.log(i, factorial);
+}
+
+// function 
+
+function factorial(n){
+    var factorial = 1;
+    for (var i = 1; i <= n; i++){
+        factorial = factorial * i;
+
+    }
+    return factorial;
+}
+var result = factorial(10);
+console.log(result);
