8704547cc4498fa12d43e07fae65d92fb25fa03c
mahadihasan1555 <mahadihasan1555@gmail.com>
2020-08-06 21:08:36 +0600

    revise and practice js leap year

--- constLet.js
+++ constLet.js
@@ -1,3 +1,8 @@
+// var = can be updated and re-declared
+// let = can be updated but can't re-declared
+// const = can neither be updated nor re-declared
+
+
 var name = "mahadi";
 console.log(name.length);
 
@@ -25,3 +30,48 @@ console.log(name2);
 
 const name1 = "mahadi";
 console.log(name1);
+
+//revise
+
+//letter count
+var name = "sahid";
+console.log(name.length);
+
+var name = "sahid hasan";
+console.log(name.length);
+
+
+var name = "sahid hasan santo";
+console.log(name.length);
+
+//condition (name change)
+var name = "sahid hasan";
+if ( name.length > 4){
+    name = "hasan santo mahadi"
+}
+console.log(name);
+
+var name = "sahid hasan";
+if ( name.length < 4){
+    name = "hasan santo mahadi"
+}
+console.log(name);
+
+
+// var = can be updated and re-declared
+// let = can be updated but can't re-declared
+// const = can neither be updated nor re-declared
+
+var pepsi = "mahadi";
+var  age = 12;
+ age = 23;
+console.log(age);
+
+
+let student = "hasan";
+ student = "hasan mahadi";
+console.log(student);
+
+const student1 = "nahadi hasan"
+student1 = "hasannn";
+console.log(student1);
--- leapYear.js
+++ leapYear.js
@@ -46,4 +46,75 @@ console.log(leapYear(1800));
 console.log(leapYear(100));
 
 
+//revise leap year 
 
+console.log( 2020 / 4); //LEAP YEAR
+console.log( 2050 / 4); // IS NOT LEAP YEAR
+console.log( 2030 / 4); // IS NOT LEAP YEAR
+
+var  leapYear = 2230;
+var reminder = leapYear % 4;
+console.log(reminder);
+
+var  leapYear = 2230;
+var reminder = leapYear % 4;
+console.log(reminder == 0);
+
+var  leapYear = 2240;
+var reminder = leapYear % 4;
+console.log(reminder == 0);
+
+
+var leapYear = 2323;
+var reminder = leapYear % 4;
+if (reminder == 0 ){
+    console.log("this is a Leap Year");
+}else{
+    console.log("this is not a Leap Year");
+}
+
+var leapYear = 2320;
+var reminder = leapYear % 4;
+if (reminder == 0 ){
+    console.log("this is a Leap Year");
+}else{
+    console.log("this is not a Leap Year");
+}
+
+//function check leap year
+
+function isLeapYear(year) {
+    if (year % 400 === 0) return true;
+    if (year % 100 === 0) return false;
+    return year % 4 === 0;
+  }
+console.log(isLeapYear(2000)); 
+console.log(isLeapYear(2001));
+console.log(isLeapYear(2004));
+console.log(isLeapYear(2100));
+
+
+// function leapYear(year)
+// {
+//   return ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
+// }
+
+
+
+// function leapYear(year){
+//     var result; 
+//     year = parseInt(document.getElementById("isYear").value);
+//     if (years/400){
+//       result = true
+//     }
+//     else if(years/100){
+//       result = false
+//     }
+//     else if(years/4){
+//       result= true
+//     }
+//     else{
+//       result= false
+//     }
+//     return result
+//  }
